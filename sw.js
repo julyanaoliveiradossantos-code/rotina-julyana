dd3bad224f0d1d87cc62b475e202d6edb1d4efed
julyanaoliveiradossantos-code <julyanaoliveiradossantos@gmail.com>
2026-06-23 17:59:56 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,23 @@
-const CACHE = 'julyana-v1';
-const FILES = ['/index.html', '/manifest.json'];
+const CACHE = 'julyana-v2';
+const BASE = '/rotina-julyana';
+const FILES = [
+  BASE + '/',
+  BASE + '/index.html',
+  BASE + '/manifest.json',
+  BASE + '/icon-192.png',
+  BASE + '/icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
